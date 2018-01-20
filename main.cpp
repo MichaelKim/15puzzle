eec4a1e96189d0c18b1590c4073e4aadc6bf14f2
Michael Kim <micklepickle98@gmail.com>
2018-01-19 22:46:07 -0800

    Rewrite algorithm

--- main.cpp
+++ main.cpp
@@ -1,136 +1,197 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
-#include <unordered_map>
-#include <unordered_set>
 #include <queue>
-#include <memory>
+#include <unordered_map>
+#include <iomanip>
+#include <ctime>
+#include <fstream>
 
 using namespace std;
 
-class State {
-public:
-    vector<vector<int>> board;
-    int blankx;
-    int blanky;
-    int dist;
+typedef unordered_map<string, int> Database;
 
-    State() {}
+enum class Direction {N, E, S, W};
 
-    State(const State& s) {
-        this->board = s.board;
-        this->blankx = s.blankx;
-        this->blanky = s.blanky;
-        this->dist = s.dist;
-    }
+struct Point {
+    int x, y;
+};
 
-    State(vector<vector<int>> newBoard, int newDist = 0) {
-        this->dist = newDist;
-        this->board = newBoard;
-        for(int i = 0; i < newBoard.size(); i++) {
-            for(int j = 0; j < newBoard[i].size(); j++) {
-                if (newBoard[i][j] == -1) {
-                    this->blankx = j;
-                    this->blanky = i;
-                    return;
+struct Board {
+    vector<vector<int>> grid;
+    vector<Point> cells;
+
+    Board(vector<vector<int>> grid): grid(grid) {
+        for (int y = 0; y < grid.size(); y++) {
+            for (int x = 0; x < grid[y].size(); x++) {
+                if (grid[y][x] > 0) {
+                    cells.push_back({x, y});
                 }
             }
         }
     }
 
-    string getID(bool blank) { // blank: true => ignore blanks
-        string boardID = "";
-        vector<int> arr;
-        for(auto row: this->board) {
-            for(int i: row) {
-                if (blank && i == -1) boardID += "0 ";
-                else boardID += to_string(i) + " ";
+    string getId() {
+        string str = "";
+
+        for (Point cell: cells) {
+            if (str.length() > 0) {
+                str += " ";
             }
+            str += to_string(cell.x) + " " + to_string(cell.y);
         }
-        return boardID;
+
+        return str;
     }
 
-    vector<shared_ptr<State>> getChildren() {
-        vector<shared_ptr<State>> c;
-        if (blanky > 0) { // Shift up
-            shared_ptr<State> up(new State(*this));
+    bool canShift(int index, Direction dir) {
+        int cellX = cells[index].x;
+        int cellY = cells[index].y;
 
-            if (up->board[this->blanky - 1][this->blankx] > 0) {
-                up->dist++;
+        if (dir == Direction::N) {
+            if (cellY > 0 && grid[cellY - 1][cellX] == 0) {
+                return true;
             }
-            swap(up->board[this->blanky][this->blankx], up->board[this->blanky - 1][this->blankx]);
-            up->blanky--;
-
-            c.push_back(up);
         }
-        if (blankx > 0) {
-            shared_ptr<State> left(new State(*this));
-
-            if (left->board[this->blanky][this->blankx - 1] > 0) {
-                left->dist++;
+        else if (dir == Direction::E) {
+            if (cellX < grid[cellY].size() - 1 && grid[cellY][cellX + 1] == 0) {
+                return true;
             }
-            swap(left->board[this->blanky][this->blankx], left->board[this->blanky][this->blankx - 1]);
-            left->blankx--;
-
-            c.push_back(left);
         }
-        if (blanky < this->board.size() - 1) {
-            shared_ptr<State> down(new State(*this));
-
-            if (down->board[this->blanky][this->blankx - 1] > 0) {
-                down->dist++;
+        else if (dir == Direction::S) {
+            if (cellY < grid.size() - 1 && grid[cellY + 1][cellX] == 0) {
+                return true;
             }
-            swap(down->board[this->blanky][this->blankx], down->board[this->blanky + 1][this->blankx]);
-            down->blanky++;
-
-            c.push_back(down);
         }
-        if (blankx < this->board[0].size() - 1) {
-            shared_ptr<State> right(new State(*this));
-
-            if (right->board[this->blanky][this->blankx - 1] > 0) {
-                right->dist++;
+        else {
+            if (cellX > 0 && grid[cellY][cellX - 1] == 0) {
+                return true;
             }
-            swap(right->board[this->blanky][this->blankx], right->board[this->blanky][this->blankx + 1]);
-            right->blankx++;
-
-            c.push_back(right);
         }
-        return c;
+
+        return false;
+    }
+
+    string getShiftId(int index, Direction dir) {
+        if (!canShift(index, dir)) return "";
+
+        string shiftId = "";
+
+        if (dir == Direction::N) {
+            cells[index].y--;
+            shiftId = getId();
+            cells[index].y++;
+        }
+        else if (dir == Direction::E) {
+            cells[index].x++;
+            shiftId = getId();
+            cells[index].x--;
+        }
+        else if (dir == Direction::S) {
+            cells[index].y++;
+            shiftId = getId();
+            cells[index].y--;
+        }
+        else {
+            cells[index].x--;
+            shiftId = getId();
+            cells[index].x++;
+        }
+
+        return shiftId;
+    }
+
+    // Assumes canShift(index, dir)
+    void shiftCell(int index, Direction dir) {
+        int cellX = cells[index].x;
+        int cellY = cells[index].y;
+
+        if (dir == Direction::N) {
+            cells[index].y--;
+            swap(grid[cellY][cellX], grid[cellY - 1][cellX]);
+        }
+        else if (dir == Direction::E) {
+            cells[index].x++;
+            swap(grid[cellY][cellX], grid[cellY][cellX + 1]);
+        }
+        else if (dir == Direction::S) {
+            cells[index].y++;
+            swap(grid[cellY][cellX], grid[cellY + 1][cellX]);
+        }
+        else {
+            cells[index].x--;
+            swap(grid[cellY][cellX], grid[cellY][cellX - 1]);
+        }
     }
 };
 
-void generateDatabase(vector<vector<int>> startBoard, string filename) {
-    unordered_set<string> checked;
-    unordered_map<string, int> database;
+ostream& operator<<(ostream& out, const Board& board) {
+    for (vector<int> row: board.grid) {
+        for (int i: row) {
+            out << setw(3) << i;
+        }
+        out << endl;
+    }
+    return out;
+}
 
-    shared_ptr<State> state (new State(startBoard));
+struct State {
+    Board board;
+    int dist;
+};
 
-    queue<shared_ptr<State>> bfs;
-    bfs.push(state);
+void generateDatabase(vector<vector<int>> grid, string filename) {
+    // board has 0 for irrelevant cells, non-0 for those part of the database
+    int count = 0;
+    int dist = 0;
+
+    clock_t startTime, test;
+    double duration;
+
+    startTime = clock();
+
+    // Start of algorithm
+    Database database;
+    queue<State> bfs;
+    Board start(grid);
+
+    cout << "Generating database for: " << endl;
+    cout << start << endl;
+
+    bfs.push({start, 0});
+    database[start.getId()] = 0;
 
     while(!bfs.empty()) {
-        shared_ptr<State> curr = bfs.front();
+        State curr = bfs.front();
         bfs.pop();
 
-        string stateID = curr->getID(false);
-        auto it = checked.find(stateID);
-        if (it == checked.end()) {
-            checked.insert(stateID);
+        // Logging
+        if (curr.dist > dist) {
+            cout << dist << ": " << count << endl;
+            dist = curr.dist;
+            count = 1;
+        }
+        else count++;
 
-            vector<shared_ptr<State>> children = curr->getChildren();
-            for(auto c: children) {
-                bfs.push(c);
+        for (int i = 0; i < curr.board.cells.size(); i++) {
+            for (int j = 0; j < 4; j++) {
+                Direction dir = static_cast<Direction>(j);
+                string shiftId = curr.board.getShiftId(i, dir);
+                if (shiftId.length() > 0 && database.find(shiftId) == database.end()) {
+                    State next = {curr.board, curr.dist + 1};
+                    next.board.shiftCell(i, dir);
+
+                    database[shiftId] = next.dist;
+
+                    bfs.push(next);
+                }
             }
         }
-
-        string stateID2 = curr->getID(true);
-        auto it2 = database.find(stateID2);
-        if (it2 == database.end() || database[stateID2] > curr->dist) {
-            database[stateID2] = curr->dist;
-            cout << stateID2 << " " << database.size() << endl;
-        }
     }
+
+    duration = (clock() - startTime) / (double)CLOCKS_PER_SEC;
+    cout<< "Time taken: " << duration << endl;
+
+    cout << "Size of database: " << database.size() << endl;
 
     // Store file
     ofstream file("database-" + filename + ".txt");
@@ -146,31 +207,32 @@ void generateDatabase(vector<vector<int>> startBoard, string filename) {
     file.close();
 }
 
+
 int main() {
-    cout << "Generating a disjoint pattern database (6-6-3):" << endl;
+    cout << "Generating a disjoint pattern database (5-5-5):" << endl;
 
-/*    cout << "Generating 6 tile database (left):" << endl;
+    cout << "Generating 5 tile database (top-left):" << endl;
     generateDatabase({
-             {1, 0, 0, 0},
+             {1, 2, 3, 0},
              {5, 6, 0, 0},
-             {9, 10, 0, 0},
-             {13, 0, 0, -1}
-    }, "6a");
-
-    cout << "Generating 6 tile database (right):" << endl;
-    generateDatabase({
              {0, 0, 0, 0},
+             {0, 0, 0, 0}
+    }, "5a2");
+
+    /*cout << "Generating 5 tile database (right):" << endl;
+    generateDatabase({
+             {0, 0, 0, 4},
              {0, 0, 7, 8},
              {0, 0, 11, 12},
-             {0, 14, 15, -1}
-    }, "6b");*/
-
-    cout << "Generating 3 tile database:" << endl;
-    generateDatabase({
-             {0, 2, 3, 4},
-             {0, 0, 0, 0},
-             {0, 0, 0, 0},
              {0, 0, 0, -1}
-    }, "3");
+    }, "5b");*/
+
+    /*cout << "Generating 5 tile database (bottom):" << endl;
+    generateDatabase({
+             {0, 0, 0, 0},
+             {0, 0, 0, 0},
+             {9, 10, 0, 0},
+             {13, 14, 15, -1}
+    }, "5c");*/
     return 0;
 }
