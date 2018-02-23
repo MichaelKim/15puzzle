953e0be57bd7df142db77378aad1d0eb1812691b
Michael Kim <micklepickle98@gmail.com>
2018-02-22 16:20:23 -0800

    Log number of nodes expanded

--- include/Idastar.h
+++ include/Idastar.h
@@ -14,6 +14,7 @@ class Idastar {
         vector<int> path;
         int minCost;
         int limit;
+        uint64_t nodes;
 
     public:
         Idastar(DisjointDatabase* d);
--- src/Idastar.cpp
+++ src/Idastar.cpp
@@ -6,24 +6,25 @@
 
 using namespace std;
 
-Idastar::Idastar(DisjointDatabase* d): db(d), path({}), minCost(INF), limit(0) {}
+Idastar::Idastar(DisjointDatabase* d): db(d), path({}), minCost(INF), limit(0), nodes(0) {}
 
 vector<int> Idastar::solve(Board start) {
     // Uses IDA* with additive pattern disjoint database heuristics
     path.clear();
 
     limit = db->getHeuristic(start);
-    cout << "Limit:";
+    cout << "Limit, Nodes:";
 
     int prevMove = -1;
     while (path.size() == 0) {
         minCost = INF;
-        cout << " " << limit << endl;
+        cout << " " << limit << ", " << nodes << endl;
         dfs(start, 0, prevMove);
         limit = minCost;
     }
 
-    cout << endl;
+    cout << "Nodes expanded: " << nodes << endl;
+    
     return path;
 }
 
@@ -41,7 +42,7 @@ bool Idastar::dfs(Board &node, int g, int prevMove) {
         }
         return false;
     }
-
+    nodes++;
     const vector<int>& moves = node.getMoves(prevMove);
     for (int move: moves) {
         node.applyMove(move);
